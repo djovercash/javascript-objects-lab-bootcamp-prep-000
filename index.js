21b009654c902a1c39bc015f4d98549b1d7c14e1
Drew Overcash <drewovercash@gmail.com>
2017-11-05 15:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var recipes = {};
 function updateObjectWithKeyAndValue (object, key, value) {
   var obj = { prop: 1 };
   Object.assign({}, obj, {[prop2]: 2});
-  return updateObjectWithKeyAndValue;
+  return updateObjectWithKeyAndValue(obj, 'prop2', 2);
 };
